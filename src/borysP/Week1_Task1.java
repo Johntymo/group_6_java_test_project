a9d4fb8124ad963e8a54c11682cdff36b5b1fbe7
bpalma91 <boryspalma@gmail.com>
2025-08-03 10:31:32 -0400

    Borys added package

---
+++ src/borysP/Week1_Task1.java
@@ -0,0 +1,16 @@
+package borysP;
+
+public class Week1_Task1 {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+/*
+/*Write  a method which can identify given number is even or odd
+         Output ex:
+         identify(5); ->"Odd"
+         identify(6); ->"Even"
+
+ */
