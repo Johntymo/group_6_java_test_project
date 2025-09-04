02e81d1a6c0022ee5f781b3cd6b1d6ec3ab3ffb9
TroyYC <troy@cydeo.com>
2025-09-04 13:46:31 -0400

    Week06 task added nev change

--- src/instructor/Week06_RemoveDuplicates.java
+++ src/instructor/Week06_RemoveDuplicates.java
@@ -6,8 +6,10 @@ import java.util.LinkedHashSet;
 public class Week06_RemoveDuplicates {
     /*
     Write function that can remove the duplicated values from String
-            Ex: removeDup("AAABBBCCC") ==> ABC
+            Ex: removeDup("AAABBBCCCAABBCCDD") ==> ABC
      */
+
+      // Joseph is volunteer
 
 
 
